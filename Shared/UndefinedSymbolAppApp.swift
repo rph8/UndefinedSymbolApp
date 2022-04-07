308071d9ed1094c586412a201b5a71bcf81342b0
Raphael Haase <raphael@sylvilagus.com>
2022-04-07 23:43:12 +0200

    Initial Commit

---
+++ Shared/UndefinedSymbolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndefinedSymbolAppApp.swift
+//  Shared
+//
+//  Created by Raphael Haase on 07.04.22.
+//
+
+import SwiftUI
+
+@main
+struct UndefinedSymbolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
